24017cda310e392d6ad453c1c3f6acbe2b60fb65
Eliel Taskinen <132335968+ElielSkillwell@users.noreply.github.com>
2024-11-19 10:25:22 +0200

    Prompt engineered a better summarize to give better tips

--- BACKEND/utils/aai_funcs.py
+++ BACKEND/utils/aai_funcs.py
@@ -9,7 +9,9 @@ def get_transcription_obj(filepath) -> aai.Transcript:
     )
 
     lemur_resp = transcript.lemur.summarize(
-        context='Please list all the filler words used, and give tips on the transcription.',
-        answer_format='A simple title with Heading 1,a paragraph with the tips for the speaker, and a list of the filler words under their own heading.'
+        # context='Please list all the filler words used, and give tips on the transcription.',
+        context='I am preparing to give a speech, and I need practical tips to make it engaging and impactful. Could you provide advice on how to improve, and give a list of all the filler words I used?',
+        answer_format='a paragraph with the tips for the speaker, and a list of the filler words that are formatted this: Filler words: ["filler_word", ...]',
+        final_model=aai.LemurModel.claude3_5_sonnet
     ).response
     return transcript, lemur_resp
